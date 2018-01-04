e409be4df44c8b1632410c154975eaac10cc7bdf
GTTgtt <33985243+GTTgtt@users.noreply.github.com>
2018-01-04 18:21:01 +0100

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,3 +1,4 @@
+/*
 var colors = [
     'mediumorchid',
     'lightcoral',
@@ -6,21 +7,23 @@ var colors = [
     'darkslateblue'
     // add more colors here (and dont forget to add ',' on previous color)
 ];
+*/
 var color_flag = 0;
 var nav_flag = 0;
+var body;
 
 function init(){
-    
+    body = document.body;
     colorWheel();
     
     // onclick #menu-icon --> show/hide #nav
     document.getElementById('menu-icon').addEventListener('click', function(){
         if(nav_flag == 0){ // show
-            document.getElementById('nav').style.top = '101px';
+            document.getElementById('nav').style.top = '100px';
             nav_flag = 1;
         }
         else { // hide
-            document.getElementById('nav').style.top = 'calc(-100% + 101px)';
+            document.getElementById('nav').style.top = 'calc(-100% + 100px)';
             nav_flag = 0;
         }
     });
@@ -28,15 +31,15 @@ function init(){
 }
 
 function colorWheel(){
-    document.body.className = 'color' + color_flag;
+    body.className = 'color' + color_flag;
     color_flag++;
     setInterval(function(){
         //document.querySelectorAll('font')[0].style.color = colors[color_flag];
         //document.querySelectorAll('font')[1].style.color = colors[color_flag];
         //document.querySelector('#hr').style.backgroundColor = colors[color_flag];
-        document.body.className = 'color' + color_flag;
+        body.className = 'color' + color_flag;
         
-        if(color_flag == colors.length - 1) color_flag= 0;
+        if(color_flag == 4) color_flag= 0;
         else color_flag++;
     }, 3000);
 }
